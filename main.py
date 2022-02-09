72da463a910d410d87c99db1942768e60e753953
Ibza Sujau <ibza.sujau@gmail.com>
2022-02-09 12:55:34 +0500

    Added cost per person for extra attractions.

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ from datetime import timedelta, date
 # TASK 1
 
 # TYPES ([0] = ticket name, [1] = one day cost, [2] two day cost)
-type_1 = ["one adult", 20, 30]
-type_2 = ["one child (an adult may bring up to two children)", 12, 18]
-type_3 = ["one senior", 16, 24]
-type_4 = ["family ticket (up to two adults or seniors, and three children)", 60, 90]
-type_5 = ["groups of six people or more, price per person", 15, 22.5]
+type_1 = ["1. one adult", 20, 30]
+type_2 = ["2. one child (an adult may bring up to two children)", 12, 18]
+type_3 = ["3. one senior", 16, 24]
+type_4 = ["4. family ticket (up to two adults or seniors, and three children)", 60, 90]
+type_5 = ["5. groups of six people or more, price per person", 15, 22.5]
 
 # EXTRA ATTRACTIONS ([0] = attraction name, [1] = cost per person)
-att_1 = ["lion feeding", 2.5]
-att_2 = ["penguin feeding", 2]
-att_3 = ["evening barbecue", 5]
+att_1 = ["1. lion feeding", 2.5]
+att_2 = ["2. penguin feeding", 2]
+att_3 = ["3. evening barbecue (two-day tickets only)", 5]
 
 table_types = [["Ticket Type", "1 Day Cost ($)", "2 Day Cost ($)"], [type_1[0], type_1[1], type_1[2]], [type_2[0], type_2[1], type_2[2]], [type_3[0], type_3[1], type_3[2]], [type_4[0], type_4[1], type_4[2]],[type_5[0], type_5[1], type_5[2]]]
-table_att = [["Extra Attraction", "Cost Per Person ($)"], [att_1[0], att_1[1]], [att_2[0], att_2[1]], [att_1[0], att_1[1]]]
+table_att = [["Extra Attraction", "Cost Per Person ($)"], [att_1[0], att_1[1]], [att_2[0], att_2[1]], [att_3[0], att_3[1]]]
 
 print(tabulate(table_types, headers='firstrow', tablefmt='fancy_grid'))
 print(tabulate(table_att, headers='firstrow', tablefmt='fancy_grid'))
@@ -40,7 +40,7 @@ def booking():
         day = int(input("Enter the day of the month you want to book: "))
         date_booked = datetime.date(2022, month, day)
 
-        if date_booked > date and date_booked < end_date:
+        if date_booked >= date and date_booked <= end_date:
             print("Confirmed date to " + str(date_booked) + ".")
         else:
             print("Your chosen date is not in range. Enter a valid date.")
@@ -56,18 +56,25 @@ def booking():
         while extra_att > 3 or extra_att < 0:
             extra_att = int(input("Select your extra attractions (0, 1, 2, 3): "))
 
+    adults = int(input("Enter the number of adults you have in your group: "))
+    children = int(input("Enter the number of children you have in your group: "))
+    seniors = int(input("Enter the number of seniors you have in your group: "))
+
     # NO EXTRA ATTRACTION
     if extra_att == 0:
         print(price)
     # LION FEEDING
     if extra_att == 1:
-        price += 2.5
+        price += 2.5 * (adults + children + seniors)
     # PENGUIN FEEDING
     if extra_att == 2:
-        price += 2
+        price += 2 * (adults + children + seniors)
     # EVENING BARBECUE (two-day tickets)
     if extra_att == 3:
-        price += 5
+        price += 5 * (adults + children + seniors)
     print(price)
 
 booking()
+
+
+
