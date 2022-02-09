4ba46731e4bd079adcbc0ad0f64117fe5206d4eb
Ibza Sujau <ibza.sujau@gmail.com>
2022-02-09 14:26:05 +0500

    Final details and cleaning up.

--- main.py
+++ main.py
@@ -4,27 +4,26 @@ from tabulate import tabulate
 from datetime import timedelta, date
 
 # TASK 1
+def show_options():
+    # TYPES ([0] = ticket name, [1] = one day cost, [2] two day cost)
+    type_1 = ["1. one adult", 20, 30]
+    type_2 = ["2. one child (an adult may bring up to two children)", 12, 18]
+    type_3 = ["3. one senior", 16, 24]
+    type_4 = ["4. family ticket (up to two adults or seniors, and three children)", 60, 90]
+    type_5 = ["5. groups of six people or more, price per person", 15, 22.5]
 
-# TYPES ([0] = ticket name, [1] = one day cost, [2] two day cost)
-type_1 = ["1. one adult", 20, 30]
-type_2 = ["2. one child (an adult may bring up to two children)", 12, 18]
-type_3 = ["3. one senior", 16, 24]
-type_4 = ["4. family ticket (up to two adults or seniors, and three children)", 60, 90]
-type_5 = ["5. groups of six people or more, price per person", 15, 22.5]
+    # EXTRA ATTRACTIONS ([0] = attraction name, [1] = cost per person)
+    att_1 = ["1. lion feeding", 2.5]
+    att_2 = ["2. penguin feeding", 2]
+    att_3 = ["3. evening barbecue (two-day tickets only)", 5]
 
-# EXTRA ATTRACTIONS ([0] = attraction name, [1] = cost per person)
-att_1 = ["1. lion feeding", 2.5]
-att_2 = ["2. penguin feeding", 2]
-att_3 = ["3. evening barbecue (two-day tickets only)", 5]
+    table_types = [["Ticket Type", "1 Day Cost ($)", "2 Day Cost ($)"], [type_1[0], type_1[1], type_1[2]], [type_2[0], type_2[1], type_2[2]], [type_3[0], type_3[1], type_3[2]], [type_4[0], type_4[1], type_4[2]],[type_5[0], type_5[1], type_5[2]]]
+    table_att = [["Extra Attraction", "Cost Per Person ($)"], [att_1[0], att_1[1]], [att_2[0], att_2[1]], [att_3[0], att_3[1]]]
 
-table_types = [["Ticket Type", "1 Day Cost ($)", "2 Day Cost ($)"], [type_1[0], type_1[1], type_1[2]], [type_2[0], type_2[1], type_2[2]], [type_3[0], type_3[1], type_3[2]], [type_4[0], type_4[1], type_4[2]],[type_5[0], type_5[1], type_5[2]]]
-table_att = [["Extra Attraction", "Cost Per Person ($)"], [att_1[0], att_1[1]], [att_2[0], att_2[1]], [att_3[0], att_3[1]]]
-
-print(tabulate(table_types, headers='firstrow', tablefmt='fancy_grid'))
-print(tabulate(table_att, headers='firstrow', tablefmt='fancy_grid'))
+    print(tabulate(table_types, headers='firstrow', tablefmt='fancy_grid'))
+    print(tabulate(table_att, headers='firstrow', tablefmt='fancy_grid'))
 
 # TASK 2
-
 def booking():
     # VARIABLES
     date = datetime.date.today()
@@ -126,6 +125,7 @@ def booking():
     if confirmation == 0:
         print("Too bad you cannot turn back now. Booking confirmed.")
 
+show_options()
 booking()
 
 
